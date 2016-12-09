10d1040a5b182f7cf40e480ff627a29db9b8e2cd
Tobias <toobew@users.noreply.github.com>
2016-12-09 15:22:32 +0100

    First simple game loop created

--- src/RunGame.hs
+++ src/RunGame.hs
@@ -1,3 +1,30 @@
 module RunGame where
 
-  
+import Data.Char
+import DataTypes
+
+data Interface = Interface
+    { iNewGame   :: Game
+    , iPrintGame :: Game -> IO ()
+    , iWinnerIs :: Game -> Player
+    , iGameOver :: Game -> Bool
+    , iShoot :: Board -> Position -> Board
+    }
+
+runGame :: Interface -> IO ()
+runGame i = do
+  putStrLn "Welcome"
+  gameLoop i (iNewGame i)
+
+gameLoop :: Interface -> Game -> IO ()
+gameLoop i (Game b1 b2) = do
+  iPrintGame i (Game b1 b2)
+  if iGameOver i (Game b1 b2) then do
+      undefined
+    else do
+      putStrLn "choose position to shoot [x y]"
+      answer <- getLine
+      let x = digitToInt (head answer)
+          y = digitToInt (last answer)
+          updb2 = iShoot i b2 (Position x y)
+      gameLoop i (Game b1 updb2)
