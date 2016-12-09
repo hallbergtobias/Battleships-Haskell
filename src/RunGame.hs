fb75fd1ef198c129ab6565d31f5d9ac945df9527
Tobias <toobew@users.noreply.github.com>
2016-12-09 16:53:57 +0100

    Now loops until it receives valid input

--- src/RunGame.hs
+++ src/RunGame.hs
@@ -11,20 +11,35 @@ data Interface = Interface
     , iShoot :: Board -> Position -> Board
     }
 
+-- starts game
 runGame :: Interface -> IO ()
 runGame i = do
-  putStrLn "Welcome"
+  putStrLn "Battleships"
   gameLoop i (iNewGame i)
 
+-- loops game until someone wins
 gameLoop :: Interface -> Game -> IO ()
 gameLoop i (Game b1 b2) = do
   iPrintGame i (Game b1 b2)
   if iGameOver i (Game b1 b2) then do
-      undefined
+      putStrLn ("Game over! " ++ show (iWinnerIs i (Game b1 b2)) ++ " won!")
     else do
-      putStrLn "choose position to shoot [x y]"
-      answer <- getLine
-      let x = digitToInt (head answer)
-          y = digitToInt (last answer)
+      answer <- validInput
+      let (x,y) = getPosition answer
           updb2 = iShoot i b2 (Position x y)
       gameLoop i (Game b1 updb2)
+      where getPosition :: String -> (Int,Int)
+            getPosition answer = (digitToInt (head answer),digitToInt (head(drop 2 answer)))
+
+-- returns input in the format "x y"
+validInput :: IO String
+validInput = do
+   putStrLn "choose position to shoot [x y]"
+   answer <- getLine
+   if (length answer == 3) && isDigit (head answer)
+     && isDigit (head (drop 2 answer))
+     then return answer
+     else do
+       putStrLn "didn't quite catch that..."
+       h <- validInput
+       return h
